d54d8559f43ebce3703906ce15ad40a0cb2b3bb9
Leonardo Lovera <leonard.lover@gmail.com>
2022-07-26 12:13:52 -0400

    Separated error.c and handled button presses

--- huygens.c
+++ huygens.c
@@ -1,49 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 //#include <SDL2/SDL_ttf.h>
 //#include <SDL2/SDL_mixer.h>
 
+#include "errors.c"
+
 #define WIDTH 1280
 #define HEIGHT 720
-
-#define SPEED 300
-
-void printError(char *prompt)
-{
-	printf("%s: %s\n", prompt, SDL_GetError());
-}
-
-int initError(void)
-{
-	printError("Initialization error");
-	return 1;
-}
-
-int windowError(void)
-{
-	printError("Window error");
-	SDL_Quit();
-	return 1;
-}
-
-int rendererError(SDL_Window *win)
-{
-	printError("Renderer error");
-	SDL_DestroyWindow(win);
-	SDL_Quit();
-	return 1;
-}
-
-int textureError(SDL_Renderer *rend, SDL_Window *win)
-{
-	printError("Texture error");
-	SDL_DestroyRenderer(rend);
-	SDL_DestroyWindow(win);
-	SDL_Quit();
-	return 1;
-}
 
 SDL_Texture *loadTexture(char *path, SDL_Renderer *rend)
 {
@@ -56,6 +22,15 @@ SDL_Texture *loadTexture(char *path, SDL_Renderer *rend)
 	SDL_FreeSurface(surf);
 
 	return tex;
+}
+
+int cursorOverRect(SDL_Rect rect, int x, int y)
+{
+	if (rect.x <= x && x <= rect.x + rect.w &&
+	    rect.y <= y && y <= rect.y + rect.h)
+			return 1;
+
+	return 0;
 }
 
 int main(void)
@@ -86,27 +61,117 @@ int main(void)
 	if (!bg)
 		return textureError(rend, win);
 
+	SDL_Texture *title = loadTexture("media/img/title.png", rend);
+
+	if (!title)
+		return textureError(rend, win);
+
+	SDL_Rect pos;
+
+	if (SDL_QueryTexture(title, NULL, NULL, &pos.w, &pos.h) != 0)
+		return queryError(title, rend, win);
+
+	pos.w *= 3;
+	pos.h *= 3;
+	pos.x = (WIDTH - pos.w) / 2;
+	pos.y = -pos.h;
+
+	SDL_Texture *stars[20];
+	SDL_Rect starsPos[20];
+
+	for (int i = 0; i < 20; i++) {
+		stars[i] = loadTexture("media/img/star.png", rend);
+
+		if (!stars[i])
+			return textureError(rend, win);
+
+		if (SDL_QueryTexture(stars[i], NULL, NULL, &starsPos[i].w, &starsPos[i].h) != 0)
+			return queryError(stars[i], rend, win);
+
+		starsPos[i].w *= 5;
+		starsPos[i].h *= 5;
+		starsPos[i].x = rand() % WIDTH;
+		starsPos[i].y = -starsPos[i].h - rand() % HEIGHT;
+	}
+
+	SDL_Texture *button = loadTexture("media/img/heart.png", rend);
+
+	if (!button)
+		return textureError(rend, win);
+
+	SDL_Rect buttonPos;
+
+	if (SDL_QueryTexture(button, NULL, NULL, &buttonPos.w, &buttonPos.h) != 0)
+		return queryError(button, rend, win);
+
+	buttonPos.w *= 8;
+	buttonPos.h *= 8;
+	buttonPos.x = (WIDTH - buttonPos.w) / 2;
+	buttonPos.y = (HEIGHT - buttonPos.h) / 2;
+
+	int mouseX, mouseY, leftClick;
+
+	int inGame = 0;
+
 	int quit = 0;
 
 	while (!quit) {
 		SDL_Event event;
 
+		leftClick = 0;
+
 		while (SDL_PollEvent(&event)) {
 			switch (event.type) {
 			case SDL_QUIT:
 				quit = 1;
+				break;
+			case SDL_MOUSEBUTTONDOWN:
+				if (event.button.button == SDL_BUTTON_LEFT)
+					leftClick = 1;
 			default:
 				break;
 			}
 		}
 
-		SDL_RenderClear(rend);
-		SDL_RenderCopy(rend, bg, NULL, NULL);
-		SDL_RenderPresent(rend);
+		SDL_GetMouseState(&mouseX, &mouseY);
+
+		if (cursorOverRect(buttonPos, mouseX, mouseY) && leftClick)
+			inGame = 1;
+		if (!inGame) {
+			pos.y += 200 / 60;
+
+			if (pos.y >= pos.h / 2)
+				pos.y = pos.h / 2;
+
+			for (int i = 0; i < 20; i++) {
+				starsPos[i].y += 150 / 60;
+
+				if (starsPos[i].y >= HEIGHT)
+					starsPos[i].y = HEIGHT;
+			}
+
+			SDL_RenderClear(rend);
+			SDL_RenderCopy(rend, bg, NULL, NULL);
+			SDL_RenderCopy(rend, title, NULL, &pos);
+			SDL_RenderCopy(rend, button, NULL, &buttonPos);
+		
+			for (int i = 0; i < 20; i++)
+				SDL_RenderCopy(rend, stars[i], NULL, &starsPos[i]);
+
+			SDL_RenderPresent(rend);
+		} else {
+			SDL_RenderClear(rend);
+			SDL_RenderPresent(rend);
+		}
 
 		SDL_Delay(1000 / 60);
 	}
 
+	for (int i = 0; i < 20; i++)
+		SDL_DestroyTexture(stars[i]);
+
+	SDL_DestroyTexture(button);
+	SDL_DestroyTexture(title);
 	SDL_DestroyTexture(bg);
 	SDL_DestroyRenderer(rend);
 	SDL_DestroyWindow(win);
